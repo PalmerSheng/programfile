5e91f533a825d4c0447fb503b73b747addb43d8f
PalmerSheng <2359245812@qq.com>
2020-09-18 15:31:00 +0800

    js常用方法更新

--- js常用方法.js
+++ js常用方法.js
@@ -1,5 +1,9 @@
+//格式化日期 默认hhhhh:mm:ss:S
 function timeFormat(date,fmt) 
 {
+	if(!fmt){
+	fmt = "yyyy-MM-dd";
+	}
 //yyyy-MM-dd hhhhh:mm:ss:S
  var o = { 
  "M+" : date.getMonth()+1,     //月份
@@ -17,4 +21,28 @@ function timeFormat(date,fmt)
  fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length))); 
  return fmt; 
 }
-timeFormat(new Date(),"yyyy-MM-dd hh:mm:ss");
+
+//验证qq号 至少5位
+function isqq(str){ var result=str.match(/[1-9][0-9]{4,}/); if(result==null) return false; return true; }
+//校验数字 输入一个字符校验一次
+function keyUp(obj) {if(obj.value.length==1){obj.value=obj.value.replace(/[^1-9]/g,'')}else obj.value=obj.value.replace(/\D/g,'')}
+//校验金额 输入金额最多为两位小数(且为正数)
+function checkAmount(amount) { var exp = /^(([1-9]\d*)|\d)(\.\d{1,2})?$/; if(amount) if(!exp.test(amount)) return false;else return true;else  return false; }	
+//校验数组 输入可以为正数
+function isNumber(str){return !isNaN(str);}
+//校验整数 可为正负
+function isInt(str){var result=str.match(/^(-|\+)?\d+$/);if(result==null) return false;return true;}
+//校验长格式日期 YYYY-MM-DD HH:MM:SS" || "YYYY/MM/DD HH:MM:SS
+function isDatetime(str){var result=str.match(/^(\d{4})(-|\/)(\d{1,2})\2(\d{1,2}) (\d{1,2}):(\d{1,2}):(\d{1,2})$/);if(result==null) return false;var d= new Date(result[1], result[3]-1, result[4], result[5], result[6], result[7]);return (d.getFullYear()==result[1]&&(d.getMonth()+1)==result[3]&&d.getDate()==result[4]&&d.getHours()==result[5]&&d.getMinutes()==result[6]&&d.getSeconds()==result[7]);}
+//校验格式日期 YYYY-MM-DD || YYYY/MM/DD
+function isDate(str){var result=str.match(/^(\d{4})(-|\/)(\d{1,2})\2(\d{1,2})$/);if(result==null) return false;var d=new Date(result[1], result[3]-1, result[4]);return (d.getFullYear()==result[1] && d.getMonth()+1==result[3] && d.getDate()==result[4]);}
+//校验邮箱
+function isEmail(str){var result=str.match(/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/);if(result==null) return false;return true;}
+//校验邮政编码
+function isPostcode(str){var result=str.match(/[1-9]\d{5}/);if(result==null) return false;return true;}
+
+//去掉最前面或最后面的空格
+function trim(str){return str.replace(/(^\s*)|(\s*$)/g, "");}
+//计算字符串长度 字符算一个
+function strlen(str){return str.replace(/[^\x00-\xff]/g, "*").length;}
+//
